f5adc163a1069eeb97e4bd27f00a83b28ee1ad44
Maless-art <franklinmjj1@gmail.com>
2026-07-25 09:47:45 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ function hideSplash() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(hideSplash, 4500);
+  setTimeout(hideSplash, 3500);
 });
 
 if('serviceWorker'in navigator)navigator.serviceWorker.register('sw.js');
